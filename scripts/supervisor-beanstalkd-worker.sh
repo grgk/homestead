55f16dbc10edb8e7bb59a6d7f80203cefecadc14
Greg <gfx@karpiak.pl>
2017-03-08 09:40:06 +0100

    supervisor worker script added

---
+++ scripts/supervisor-beanstalkd-worker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+worker="[program:worker-1]
+command=php /home/vagrant/app/surplace/artisan queue:work beanstalkd --sleep=10 --daemon --quiet --env=production --queue="default"
+
+autostart=true
+autorestart=true
+user=vagrant
+redirect_stderr=true
+stdout_logfile=/home/vagrant/app/worker-1.log
+"
+
+echo "$worker" > "/etc/supervisor/conf.d/worker-1.conf"
+sudo supervisorctl restart all
