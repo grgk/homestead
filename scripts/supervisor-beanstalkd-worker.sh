2ab79cc2ef63dab882eb502679b96dac12b55af7
Greg <gfx@karpiak.pl>
2018-02-15 13:03:20 +0100

    Supervisor configuration updated

--- scripts/supervisor-beanstalkd-worker.sh
+++ scripts/supervisor-beanstalkd-worker.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 worker="[program:worker-1]
-command=php /home/vagrant/app/ra/artisan queue:work beanstalkd --sleep=10 --daemon --quiet --tries=5 --env=production --queue="default"
+command=php /home/vagrant/app/ra/artisan queue:work beanstalkd --sleep=10 --daemon --quiet --tries=3 --env=production --queue="default"
 
+process_name=%(program_name)s_%(process_num)02d
+startsecs=0
 autostart=true
 autorestart=true
+stopasgroup=true
+killasgroup=true
 user=vagrant
+numprocs=2
 redirect_stderr=true
 stdout_logfile=/home/vagrant/app/worker-1.log
 "
